d86a23f48a61ab7f35d860fc3a67ac18c8da3707
vivy <2686220852@qq.com>
2025-04-12 15:48:06 +0800

    add msgque_send and msgque_read but not finish

--- Core/Inc/msgque.h
+++ Core/Inc/msgque.h
@@ -12,7 +12,7 @@ typedef struct msgque {
   size_t msg_count;
   size_t msg_size;
   list_t read_waiting_list;
-  list_t write_waiting_list;
+  list_t send_waiting_list;
 } msgque_t;
 
 typedef msgque_t *msgque_handler;
@@ -20,5 +20,7 @@ typedef msgque_t *msgque_handler;
 msgque_handler msgque_create(const uint32_t length, const uint32_t msg_size);
 void msgque_delete(msgque_handler msg_que);
 uint32_t msgque_send(msgque_handler target_que, const void *const msg,
-                     uint32_t wait_ticks);
+                     uint32_t block_ticks);
+uint32_t msgque_read(msgque_handler source_que, void *msg_buf,
+                     uint32_t block_ticks);
 #endif /*__MSGQUE_H*/
--- Core/Src/msgque.c
+++ Core/Src/msgque.c
@@ -21,7 +21,7 @@ msgque_handler msgque_create(const uint32_t length, const uint32_t msg_size) {
       new_que->length = length;
       new_que->msg_count = 0;
       new_que->msg_size = msg_size;
-      list_init(&(new_que->write_waiting_list));
+      list_init(&(new_que->send_waiting_list));
       list_init(&(new_que->read_waiting_list));
     }
   }
@@ -35,34 +35,77 @@ void msgque_delete(msgque_handler msg_que) {
 }
 
 uint32_t msgque_send(msgque_handler target_que, const void *const msg,
-                     uint32_t wait_ticks) {
-  uint32_t success = FALSE;
+                     uint32_t block_ticks) {
+  uint32_t timer_set = FALSE;
+  block_timer_t block_timer;
+  for (;;) {
+    uint32_t saved = critical_enter();
+    if (target_que->msg_count < target_que->length) {
+      // copy data to queue
+      critical_exit(saved);
+      return TRUE;
+    } else if (block_ticks == 0) {
+      critical_exit(saved);
+      return FALSE;
+    }
+    if (!timer_set) {
+      block_timer.start_tick = get_current_tick();
+      timer_set = TRUE;
+    }
+
+    tcb_t *current_tcb = get_current_tcb();
+    if (!block_timer_check(&block_timer, &block_ticks)) {
+      if (target_que->msg_count == target_que->length) {
+        list_insert_node(&(target_que->send_waiting_list),
+                         &(current_tcb->event_node));
+        add_tcb_to_delay_list(current_tcb, block_ticks);
+        task_switch();
+      }
+    } else {
+      critical_exit(saved);
+      return FALSE;
+    }
+    critical_exit(saved);
+  }
 
   uint32_t saved = critical_enter();
-  if (target_que->msg_count < target_que->length) {
-    memcpy(target_que->next_write, msg, target_que->msg_size);
-    target_que->next_write += target_que->msg_size;
-
-    if (target_que->next_write >= target_que->tail) {
-      target_que->next_write = target_que->front;
-    }
-
-    target_que->msg_count++;
-    success = TRUE;
-    if (!list_is_empty(&(target_que->read_waiting_list))) {
-      // unblock the task
-    }
-    critical_exit(saved);
-    return success;
-  } else if (wait_ticks > 0) {
-    // block
-
-  } else {
-    critical_exit(saved);
-    return success;
-  }
   critical_exit(saved);
+}
 
-  scheduler_suspend();
-  scheduler_resume();
+uint32_t msgque_read(msgque_handler source_que, void *msg_buf,
+                     uint32_t block_ticks) {
+  uint32_t timer_set = FALSE;
+  block_timer_t block_timer;
+  for (;;) {
+    uint32_t saved = critical_enter();
+    if (source_que->msg_count > 0) {
+      // copy data from queue
+      critical_exit(saved);
+      return TRUE;
+    } else if (block_ticks == 0) {
+      critical_exit(saved);
+      return FALSE;
+    }
+    if (!timer_set) {
+      block_timer.start_tick = get_current_tick();
+      timer_set = TRUE;
+    }
+
+    tcb_t *current_tcb = get_current_tcb();
+    if (!block_timer_check(&block_timer, &block_ticks)) {
+      if (source_que->msg_count == 0) {
+        list_insert_node(&(source_que->send_waiting_list),
+                         &(current_tcb->event_node));
+        add_tcb_to_delay_list(current_tcb, block_ticks);
+        task_switch();
+      }
+    } else {
+      critical_exit(saved);
+      return FALSE;
+    }
+    critical_exit(saved);
+  }
+
+  uint32_t saved = critical_enter();
+  critical_exit(saved);
 }
