7225505c45cb7d086a016f319ceda6dd04929f62
vivy <2686220852@qq.com>
2025-05-06 20:38:04 +0800

    adjust file struct of sh.c

--- Core/Src/sh.c
+++ Core/Src/sh.c
@@ -12,37 +12,29 @@
 #include <stdint.h>
 #include <string.h>
 extern UART_HandleTypeDef huart1;
+typedef struct {
+  uint8_t buf[configSHELL_MAX_CMD_LEN];
+  uint32_t size;
+} cmd_msg_t;
 
-int cmd_ps(int argc, char **argv) {
-  (void)argc;
-  (void)argv;
-  print_tasks(PRINT_READY);
-  print_tasks(PRINT_SUSPENDED);
-  print_tasks(PRINT_DELAY);
-  return 0;
-}
+extern DMA_HandleTypeDef hdma_usart1_rx;
+static msgque_handler sh_msgque;
+static uint8_t sh_cmd_msg_buf[sizeof(cmd_msg_t)];
+static uint8_t sh_uart_rx_buf[configSHELL_MAX_CMD_LEN];
 
-int cmd_help(int argc, char **argv) {
-  (void)argc;
-  (void)argv;
-  return 0;
-}
+static int cmd_ps(int argc, char **argv);
+static int cmd_help(int argc, char **argv);
+static int cmd_mem(int argc, char **argv);
+static void print_logo(void);
 
-int cmd_mem(int argc, char **argv) {
-  (void)argc;
-  (void)argv;
-  print_mem();
-  return 0;
-}
-
-const shcmd_t cmd_table[] = {
+static const shcmd_t cmd_table[] = {
     {"help", cmd_help, "Show all commands"},
     {"ps", cmd_ps, "Task management"},
     {"mem", cmd_mem, "Memory usage"},
     {NULL, NULL, NULL} // 结束标记
 };
 
-void shell_execute(uint8_t *buf, uint32_t len) {
+static void shell_execute(uint8_t *buf, uint32_t len) {
   int argc = 0;
   char *argv[configSHELL_MAX_ARGS];
 
@@ -66,20 +58,59 @@ void shell_execute(uint8_t *buf, uint32_t len) {
   printf_("Unknown command: %s\r\n", argv[0]);
 }
 
-typedef struct {
-  uint8_t buf[configSHELL_MAX_CMD_LEN];
-  uint32_t size;
-} sh_cmd_msg;
+void shell_task(void *arg) {
+  (void)arg;
+  sh_msgque = msgque_create(1, sizeof(cmd_msg_t));
+  memset(sh_cmd_msg_buf, 0, sizeof(cmd_msg_t));
+  HAL_UARTEx_ReceiveToIdle_DMA(&huart1, sh_uart_rx_buf,
+                               configSHELL_MAX_CMD_LEN);
+  while (1) {
+    print_logo();
+    if (msgque_recieve(sh_msgque, sh_cmd_msg_buf, MAX_DELAY)) {
+      cmd_msg_t *msg = (cmd_msg_t *)sh_cmd_msg_buf;
+      shell_execute(msg->buf, msg->size);
+    }
+  }
+}
 
-extern DMA_HandleTypeDef hdma_usart1_rx;
-static msgque_handler sh_msgque;
-static uint8_t sh_cmd_msg_buf[sizeof(sh_cmd_msg)];
-static uint8_t sh_uart_rx_buf[configSHELL_MAX_CMD_LEN];
+static int cmd_ps(int argc, char **argv) {
+  (void)argc;
+  (void)argv;
+  print_tasks(PRINT_READY);
+  print_tasks(PRINT_SUSPENDED);
+  print_tasks(PRINT_DELAY);
+  return 0;
+}
+
+static int cmd_help(int argc, char **argv) {
+  (void)argc;
+  (void)argv;
+  return 0;
+}
+
+static int cmd_mem(int argc, char **argv) {
+  (void)argc;
+  (void)argv;
+  print_mem();
+  return 0;
+}
+
+static void print_logo(void) {
+  const char *logo = "__  __ _____ _   _ _____ _______ ____   _____\n"
+                     "|  \\/  |_   _| \\ | |  __ \\__   __/ __ \\ / ____|\n"
+                     "| \\  / | | | |  \\| | |__) | | | | |  | | (___  \n"
+                     "| |\\/| | | | | . ` |  _  /  | | | |  | |\\___ \\ \n"
+                     "| |  | |_| |_| |\\  | | \\ \\  | | | |__| |____) |\n"
+                     "|_|  |_|_____|_| \\_|_|  \\_\\ |_|  \\____/|_____/ \n";
+
+  printf_("%s\n", logo);
+  return;
+}
 
 void HAL_UARTEx_RxEventCallback(UART_HandleTypeDef *huart, uint16_t Size) {
   if (huart == &huart1) {
 
-    sh_cmd_msg msg;
+    cmd_msg_t msg;
     memcpy(msg.buf, sh_uart_rx_buf, Size);
 
     msg.buf[Size] = '\0';
@@ -89,32 +120,5 @@ void HAL_UARTEx_RxEventCallback(UART_HandleTypeDef *huart, uint16_t Size) {
     HAL_UARTEx_ReceiveToIdle_DMA(&huart1, sh_uart_rx_buf,
                                  configSHELL_MAX_CMD_LEN);
     __HAL_DMA_DISABLE_IT(&hdma_usart1_rx, DMA_IT_HT);
-  }
-}
-
-void print_logo(void) {
-  const char *logo = "__  __ _____ _   _ _____ _______ ____   _____\n"
-                     "|  \\/  |_   _| \\ | |  __ \\__   __/ __ \\ / ____|\n"
-                     "| \\  / | | | |  \\| | |__) | | | | |  | | (___  \n"
-                     "| |\\/| | | | | . ` |  _  /  | | | |  | |\\___ \\ \n"
-                     "| |  | |_| |_| |\\  | | \\ \\  | | | |__| |____) |\n"
-                     "|_|  |_|_____|_| \\_|_|  \\_\\ |_|  \\____/|_____/ \n";
-
-  printf_("%s\n", logo); // 使用标准 printf
-  return;
-}
-
-void shell_task(void *arg) {
-  (void)arg;
-  sh_msgque = msgque_create(1, sizeof(sh_cmd_msg));
-  memset(sh_cmd_msg_buf, 0, sizeof(sh_cmd_msg));
-  HAL_UARTEx_ReceiveToIdle_DMA(&huart1, sh_uart_rx_buf,
-                               configSHELL_MAX_CMD_LEN);
-  while (1) {
-    print_logo();
-    if (msgque_recieve(sh_msgque, sh_cmd_msg_buf, MAX_DELAY)) {
-      sh_cmd_msg *msg = (sh_cmd_msg *)sh_cmd_msg_buf;
-      shell_execute(msg->buf, msg->size);
-    }
   }
 }
